1b091fdbabf0b2360367db6b1702a4c85949e786
KeldarWolf <144638401+KeldarWolf@users.noreply.github.com>
2026-05-13 08:38:08 -0400

    mod bd

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routers/authRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -15,6 +17,6 @@ app.get("/", (req, res) => {
     res.send("EmpatIA funcionando");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor puerto 3000");
 });
