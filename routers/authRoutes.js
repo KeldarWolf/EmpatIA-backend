a1825f7e03da72f942e05ca0082e060c334312ff
KeldarWolf <144638401+KeldarWolf@users.noreply.github.com>
2026-05-13 08:28:34 -0400

    mod config conection BD

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import pool from "../db.js";
+import pool from "../config/db.js";
 
 const router = express.Router();
 
@@ -16,18 +16,24 @@ router.post("/register", async (req, res) => {
             VALUES ($1, $2, $3, $4)
             RETURNING *
             `,
-            [nombre, edad, email, password]
+            [
+                nombre,
+                edad || null,
+                email,
+                password
+            ]
         );
 
         res.json(result.rows[0]);
 
     } catch (error) {
+
         console.error(error);
+
         res.status(500).json({
             error: "Error creando usuario"
         });
     }
-
 });
 
 export default router;
