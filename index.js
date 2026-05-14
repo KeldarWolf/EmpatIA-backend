fae72a7b444f4728fe1f3217f70cba4ae433078d
KeldarWolf <144638401+KeldarWolf@users.noreply.github.com>
2026-05-14 08:35:32 -0400

    Add chat routes to the main application

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routers/authRoutes.js";
 import usersRoutes from "./routers/usersRoutes.js";
+import chatRoutes from "./routers/chatRoutes.js";
 
 dotenv.config();
 
@@ -12,13 +13,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// logger
 app.use((req, res, next) => {
   console.log("➡️", req.method, req.url);
   next();
 });
 
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
+app.use("/chat", chatRoutes);
 
 app.get("/", (req, res) => {
   res.send("EmpatIA Backend activo 🚀");
