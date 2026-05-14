6ae32f9446b1118664dc755ade050384ba3cdb47
KeldarWolf <144638401+KeldarWolf@users.noreply.github.com>
2026-05-14 07:45:02 -0400

    Add usersRoutes and improve logging format

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoutes from "./routers/authRoutes.js";
+import usersRoutes from "./routers/usersRoutes.js";
 
 dotenv.config();
 
@@ -12,11 +13,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(`➡️ ${req.method} ${req.url}`);
+  console.log("➡️", req.method, req.url);
   next();
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
 
 app.get("/", (req, res) => {
   res.send("EmpatIA Backend activo 🚀");
